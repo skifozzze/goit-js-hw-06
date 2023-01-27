486edfac1166ee77b342660514e97bce32d5ae14
skifozzze <skifozzze@gmail.com>
2023-01-27 17:36:22 +0200

    task2

--- task-2.js
+++ task-2.js
@@ -2,3 +2,10 @@ import users from "./users.js"
 
 console.table(users)
 
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color)
+};
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+console.log(getUsersWithEyeColor(users, 'green'))
