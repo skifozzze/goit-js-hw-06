cd5a62fc3b3f34c8259352c184f22acf5b583f93
skifozzze <skifozzze@gmail.com>
2023-01-27 16:02:33 +0200

    task1 upd

--- task-1.js
+++ task-1.js
@@ -2,10 +2,9 @@ import users from "./users.js"
 
 console.log(users)
 
-const getUserNames = users.map(user => {
-  return user.name
-})
+const getUserNames = users => {
+  return users.map(user => user.name)
+};
 
-
-console.log(getUserNames);
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]  
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
