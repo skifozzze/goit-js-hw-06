42f598f0e9d9c31d12fa90fa0794b0438504d91a
skifozzze <skifozzze@gmail.com>
2023-01-30 16:53:40 +0200

    upd 7

--- task-7.js
+++ task-7.js
@@ -1,9 +1,14 @@
 import users from "./users.js"
 
 console.table(users)
+ 
+
+// const calculateTotalBalance = users =>
+// users.map(user => user.balance).reduce((acc, balance) => acc + balance, 0)
+// ;
 
 const calculateTotalBalance = users =>   
-users.map(user => user.balance).reduce((acc, balance) => acc + balance, 0)
+users.reduce((acc, user) => acc + user.balance, 0)
 ;
 
 console.log(calculateTotalBalance(users)); // 20916
