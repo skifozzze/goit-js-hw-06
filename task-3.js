a58085bd517cce7928a8566020d219da44de69a1
skifozzze <skifozzze@gmail.com>
2023-01-30 15:35:19 +0200

    task3

--- task-3.js
+++ task-3.js
@@ -3,11 +3,10 @@ import users from "./users.js"
 console.table(users)
 
 const getUsersWithGender = (users, gender) => {
-  const findGender = users.map(user => user.gender);
-  console.log(findGender)
-  // if (findGender === gender) {
-  //   return users.map(user => user.name)
-  // }
+  
+  const filteredGenders = users.filter(user => user.gender === gender);
+  
+  return filteredGenders.map(gender => gender.name)
 };
 
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
