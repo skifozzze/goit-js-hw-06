8dffd00ad24702d3de77b983fb23cf3c7eea3b67
skifozzze <skifozzze@gmail.com>
2023-02-01 12:23:01 +0200

    task10

---
+++ task-10.js
@@ -0,0 +1,21 @@
+import users from './users.js';
+
+console.table(users);
+
+// const getNamesSortedByFriendsCount = users => {
+//   const sortedFriends = [...users].sort(
+//     (prevUser, nextUser) => prevUser.friends.length - nextUser.friends.length
+//   );
+
+//   return sortedFriends.map(user => user.name);
+// };
+
+const getSortedUniqueSkills = users =>
+  users
+    .reduce((acc, user) => [...acc, ...user.skills], [])
+    .filter((skill, index, arr) => arr.indexOf(skill) === index)
+    .sort();
+
+console.log(getSortedUniqueSkills(users));
+
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
